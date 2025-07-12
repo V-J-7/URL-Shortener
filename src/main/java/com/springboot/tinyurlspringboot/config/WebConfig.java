6c0eefad6e7c1bf4a22097c198e020a847bb3c08
V-J-7 <vatsaljain231@gmail.com>
2025-07-12 18:18:14 +0530

    updated to use deployed URL

--- src/main/java/com/springboot/tinyurlspringboot/config/WebConfig.java
+++ src/main/java/com/springboot/tinyurlspringboot/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://url-shortener-ce9u.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
